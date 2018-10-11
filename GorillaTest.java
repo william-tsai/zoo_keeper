2bba2c06026ddc995dfa455697d0935e8c775008
William_Tsai <tsaiyuhlin@gmail.com>
2018-10-11 01:54:25 -0700

    first commit

---
+++ GorillaTest.java
@@ -0,0 +1,12 @@
+public class GorillaTest {
+    public static void main(String[] args) {
+        Gorilla koko = new Gorilla();
+        koko.throwSomething();
+        koko.throwSomething();
+        koko.throwSomething();
+        koko.eatBananas();
+        koko.eatBananas();
+        koko.climb();
+        koko.displayEnergy();
+    }
+}
